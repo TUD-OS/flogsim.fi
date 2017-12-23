c2db24735a68bad4c85ad335b029bf7f8261cf27
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-23 14:42:27 +0100

    Update analysis script

--- analysis/plot_faults.R
+++ analysis/plot_faults.R
@@ -2,13 +2,10 @@
 
 suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
-suppressMessages(library(dbplyr))
 suppressMessages(library(ggplot2))
 suppressMessages(library(optparse))
 
 option.list <- list(
-    make_option(c("-H", "--hostname"), default = "localhost", metavar = "HOST",
-                help = "Host where the database runs. [default: %default]"),
     make_option(c("-r", "--runtime"), default = "runtime.pdf", metavar = "FILE",
                 help = "Output PDF file with runtime analysis. [default: %default]"),
     make_option(c("-m", "--messages"), default = "messages.pdf", metavar = "FILE",
@@ -16,24 +13,27 @@ option.list <- list(
 
 opt.parser <- OptionParser(option_list = option.list,
                            usage = "%prog [options]")
-opt <- parse_args2(opt.parser)
+opt <- parse_args(opt.parser, positional_arguments = 1)
 
-flogsim.db <- src_mysql(dbname = 'flogsim', host = opt$options$hostname, user = 'user', password = 'user')
+log.dir <- opt$args
 
-plan <- tbl(flogsim.db, 'experiment_plan')
-runs <- tbl(flogsim.db, 'experiment_log')
+if (!dir.exists(log.dir)) {
+    stop(paste("Directory", log.dir, "does not exist"))
+}
 
-pdf('runtime.pdf', width = 7, height = 4)
+plan <- read.csv(file.path(log.dir, 'experiment_plan.csv'))
+runs <- read.csv(file.path(log.dir, 'experiment_logs.csv'))
+
+pdf(opt$options$runtime, width = 7, height = 4)
 
 res <- plan %>%
     inner_join(runs) %>%
-    group_by(COLL, k, L, o, g, P, F) %>%
-    summarise(avg_runtime = mean(runtime),
-              avg_msg = mean(msg_task),
-              sd_runtime = sd(runtime),
-              sd_msg = sd(msg_task)) %>%
+    group_by(COLL, k, PAR, L, o, g, P, F) %>%
+    summarise(avg_runtime = mean(TotalRuntime),
+              avg_msg = mean(MsgTask),
+              sd_runtime = sd(TotalRuntime),
+              sd_msg = sd(MsgTask)) %>%
     ungroup() %>%
-    filter(COLL != 'checked_corrected_gossip_bcast') %>%
     collect()
 
 max.runtime = max(res$avg_runtime + res$sd_runtime + 0.1)
@@ -43,16 +43,17 @@ node.list <- (2^c(1:17)) - 1
 res <- mutate(res, COLL = revalue(COLL,
                                   c("checked_corrected_binomial_bcast" = "Checked binomial",
                                     "checked_corrected_optimal_bcast" = "Checked optimal",
-                                    "phased_checked_corrected_binomial_bcast" = "Phased binomial")))
+                                    "phased_checked_corrected_binomial_bcast" = "Phased binomial",
+                                    "checked_corrected_gossip_bcast" = "Checked gossip")))
 
 p <- res %>%
-    group_by(k, L, o, g, F) %>%
+    group_by(k, PAR, L, o, g, F) %>%
     do (
         plots = ggplot(data = .) +
             geom_line(aes(x = P, y = avg_runtime, color = COLL)) +
             ylim(0, max.runtime) +
             ggtitle("Scalability at the same fault rate",
-                    subtitle = paste("F =", .$F, "%", "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k)) +
+                    subtitle = paste("F =", .$F, "%", "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k, "PAR =", .$PAR)) +
             ylab("Runtime, steps") +
             guides(color = guide_legend(title = "Collective")) +
             theme_linedraw(base_size = 10) +
@@ -75,13 +76,13 @@ print(lapply(p$plots, add.log.scale))
 p <- res %>%
     filter(F %in% c(1, 2, 4, 6, 8, 10)) %>%
     collect() %>%
-    group_by(COLL, L, o, g, k) %>%
+    group_by(L, o, g, k, PAR, COLL) %>%
     do (
         plots = ggplot(data = .) +
             geom_line(aes(x = P, y = avg_runtime, color = as.factor(F))) +
             ylim(0, max.runtime) +
             ggtitle("Sturdiness with growing number of faults",
-                    subtitle = paste(.$COLL, "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k)) +
+                    subtitle = paste(.$COLL, "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k, "PAR =", .$PAR)) +
             ylab("Runtime, steps") +
             guides(color = guide_legend(title = "F (%)")) +
             scale_color_brewer(type = "seq", palette="Oranges") +
@@ -96,18 +97,18 @@ print(lapply(p$plots, add.log.scale))
 
 dev.off()
 
-pdf('messages.pdf', width = 7, height = 4)
+pdf(opt$options$messages, width = 7, height = 4)
 
 max.msg = max(res$avg_msg + res$sd_msg + 0.1)
 
 p <- res %>%
-    group_by(k, L, o, g, F) %>%
+    group_by(k, PAR, L, o, g, F) %>%
     do (
         plots = ggplot(data = .) +
             geom_line(aes(x = P, y = avg_msg, color = COLL)) +
             ylim(0, max.msg) +
             ggtitle("Scalability at the same fault rate",
-                    subtitle = paste("F =", .$F, "%", "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k)) +
+                    subtitle = paste("F =", .$F, "%", "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k, "PAR =", .$PAR)) +
             ylab("Messages, count") +
             guides(color = guide_legend(title = "Collective")) +
             theme_linedraw(base_size = 10) +
@@ -128,13 +129,13 @@ print(lapply(p$plots, add.log.scale))
 p <- res %>%
     filter(F %in% c(1, 2, 4, 6, 8, 10)) %>%
     collect() %>%
-    group_by(COLL, L, o, g, k) %>%
+    group_by(L, o, g, k, PAR, COLL) %>%
     do (
         plots = ggplot(data = .) +
             geom_line(aes(x = P, y = avg_msg, color = as.factor(F))) +
             ylim(0, max.msg) +
             ggtitle("Sturdiness with growing number of faults",
-                    subtitle = paste(.$COLL, "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k)) +
+                    subtitle = paste(.$COLL, "L =", .$L, "o = ", .$o, "g =", .$g, "k =", .$k, "PAR =", .$PAR)) +
             ylab("Messages, count") +
             guides(color = guide_legend(title = "F (%)")) +
             scale_color_brewer(type = "seq", palette="Oranges") +
