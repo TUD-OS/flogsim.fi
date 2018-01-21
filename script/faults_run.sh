c9f37a29955f0d58879934880cbdc9492875aa4a
Maksym Planeta <mcsim.planeta@gmail.com>
2018-01-21 14:30:34 +0100

    Change setup to fit in the memory

--- script/faults_run.sh
+++ script/faults_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p_ffmk
 #SBATCH --time=70:00:00
-#SBATCH --mem-per-cpu=1500
+#SBATCH --mem-per-cpu=2400
 #SBATCH --ntasks=1
 #SBATCH --output=results-%A.%a.out
 #SBATCH --error=out-%A.%a.err
